816c1155b78164ac4fde46676db8eec8daf52523
Alessandro Dodi <dodialessandro3@gmail.com>
2022-12-14 10:31:33 +0100

    Add new meetup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import MainNavigation from './components/layout/MainNavigation'
+import Layout from './components/layout/Layout'
 import AllMeetupsPage from './pages/AllMeetup'
 import FavoritesPage from './pages/Favorites'
 import NewMeetupPage from './pages/NewMeetup'
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path="/" element={<AllMeetupsPage />}></Route>
         <Route path="/favorites" element={<FavoritesPage />}></Route>
         <Route path="/new-meetup" element={<NewMeetupPage />}></Route>
       </Routes>
-    </div>
+    </Layout>
   )
 }
 
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,12 @@
+import NewMeetupForm from '../components/meetups/NewMeetupForm'
+
 function NewMeetupPage() {
-  return <div>New meetups Page</div>
+  return (
+    <section>
+      <h1>Add new meetup</h1>
+      <NewMeetupForm></NewMeetupForm>
+    </section>
+  )
 }
 
 export default NewMeetupPage
